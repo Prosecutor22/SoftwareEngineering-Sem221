c08ce2e5194fa1872efb3cec77780dc5b6ce8f72
Quang Khánh <khanhnguyenmcu@gmail.com>
2022-11-19 11:33:28 +0700

    Add to-do

---
+++ routes/back-officer.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+// TO-DO: render dashboard
+router.get('/dashboard', function(req, res, next) {
+
+});
+
+// TO-DO: render assign task
+router.get('/assign-task', function(req, res, next) {
+
+});
+
+// TO-DO: render task history
+router.get('/task-history', function(req, res, next) {
+
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// TO-DO: render home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// TO-DO: render signin page
+router.get('/signin', function(req, res, next) {
+
+});
+
+// TO-DO: handle signin with passport.js
+router.post('/signin', function(req, res, next) {
+
+});
+
+// TO-DO: handle signout
+router.post('/signout', function(req, res, next) {
+
 });
 
 module.exports = router;
