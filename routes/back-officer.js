bba61c5af61fe46030dbf23ce5ae43205cd56343
trongphu012345@gmail.com <75671199+trongphuhappy@users.noreply.github.com>
2022-11-30 23:11:37 +0700

    task-history

--- routes/back-officer.js
+++ routes/back-officer.js
@@ -6,17 +6,25 @@ var ensureLoggedIn = ensureLogIn('/signin');
 
 // TO-DO: render dashboard
 router.get('/dashboard', ensureLoggedIn, function(req, res, next) {
-    res.send("Welcome to Dashboard!");
+    res.send("Welcome to Dashboard!"); 
 });
 
 // TO-DO: render assign task
 router.get('/assign-task', ensureLoggedIn, function(req, res, next) {
-
+    res.render('assign-task', {
+        title: 'Assign Task',
+        task: [{'route': "ABC", 'vehicle': "12345", 'assignee': "collector 5"},
+               {'route': "ABCD", 'vehicle': "123456", 'assignee': "collector 6"}]
+    })
 });
 
 // TO-DO: render task history
 router.get('/task-history', ensureLoggedIn, function(req, res, next) {
-
+    res.render('task-history', {
+        title: 'Task History',
+        history: [{'week': 1, 'janitor': '001'},
+                  {'week': 2, 'janitor': '002'}]
+    })
 });
 
 module.exports = router;
