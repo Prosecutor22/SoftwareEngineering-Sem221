268c3840e5f6d328562405c00b88b32376a7e02e
Quang Khánh <khanhnguyenmcu@gmail.com>
2022-12-03 17:13:00 +0700

    Add drag and drop in assign-task.js

--- public/js/task-history.js
+++ public/js/task-history.js
@@ -6,25 +6,3 @@
     /back-officer/task-history + url query parameters created from filters
 */
 
-var express = require('express');
-const datelib = require('date-and-time');
-const { response } = require('express');
-var router = express.Router();
-var MCP = require('../models').MCP;
-var Collector = require('../models').Collector;
-var Janitor = require('../models').Janitor;
-var Route = require('../models').Route;
-var Tasks = require('../models').Tasks;
-var weekTime = require('../models').weekTime;
-
-function renderTaskHistory(data){
-
-}
-
-fetch('/task-history')
-    .then(response => {
-        return response.json();
-    })
-    .then(response =>{
-        renderTaskHistory(response);
-    })
