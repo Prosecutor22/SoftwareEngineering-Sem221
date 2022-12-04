708bf6c6cdc941d6f46b4f222b6dcc9441d87265
HUYNH TAN LOC <huynhtanloc@MacBook-Pro-cua-HUYNH.local>
2022-12-04 09:08:36 +0700

    update task-history

--- public/js/task-history.js
+++ public/js/task-history.js
@@ -6,3 +6,43 @@
     /back-officer/task-history + url query parameters created from filters
 */
 
+const data = {
+    "onButton": "Week",
+    "hideData": ["ID", "Name", "Vehicle", "Route", "Troller", "MCP"] 
+}
+
+var tempData = data;
+
+function changeEJS(tempData){
+    const onButton = document.querySelector(".dropbtn");
+    onButton.innerText = tempData.onButton;
+    var choosetypes = document.querySelectorAll('.choosetype');
+    for (var i=0; i<6; i++){
+        choosetypes[i].innerText = (tempData.hideData)[i];
+    }
+}
+
+const handle_click = (e) => {
+    var name = e.target.innerText;
+    console.log(name);
+    onButton = tempData.onButton;
+    tempData.onButton = name;
+    for (var i=0; i<6; i++){
+        if ((tempData.hideData)[i] === name){
+            (tempData.hideData)[i] = onButton;
+        }
+    }
+    changeEJS(tempData);
+}
+
+const choosetypes = document.querySelectorAll('.choosetype');
+choosetypes.forEach((choosetype) => {
+    choosetype.addEventListener("click", handle_click);
+});
+
+const search = document.querySelector('.search');
+search.addEventListener("click", () => {
+    const url = new URL(document.location);
+    //window.location.href = url.pathname + `&q=${tempData.onButton}`;
+});
+
